ca0b480b331643dc230781669d0ad2968ca0aa37
duolabs01-dot <duolabs01@gmail.com>
2026-02-23 02:16:54 -0500

    Centralise Domain Config

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,8 @@
-export const APP_URL = process.env.NEXT_PUBLIC_APP_URL ?? 'https://centreconnect.co.za'
-export const ROOT_DOMAIN = process.env.NEXT_PUBLIC_ROOT_DOMAIN ?? 'centreconnect.co.za'
-export const SUPPORT_EMAIL = process.env.SUPPORT_EMAIL ?? 'support@centreconnect.co.za'
+export const APP_URL =
+  process.env.NEXT_PUBLIC_APP_URL ?? 'https://centerconnect.co.za'
+
+export const ROOT_DOMAIN =
+  process.env.NEXT_PUBLIC_ROOT_DOMAIN ?? 'centerconnect.co.za'
+
+export const SUPPORT_EMAIL =
+  process.env.SUPPORT_EMAIL ?? 'support@centerconnect.co.za'
