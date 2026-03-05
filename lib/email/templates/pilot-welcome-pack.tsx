ea23fb552cdbe796860939603515f54e981e16d0
duolabs01-dot <duolabs01@gmail.com>
2026-03-05 14:20:07 -0500

    fix: use email-safe CC logo asset in onboarding templates

--- lib/email/templates/pilot-welcome-pack.tsx
+++ lib/email/templates/pilot-welcome-pack.tsx
@@ -49,7 +49,7 @@ const BRAND = {
   border: '#E2E8F0',
 }
 
-const CENTRECONNECT_LOGO_URL = 'https://centerconnect.co.za/centreconnect-logo.svg'
+const CENTRECONNECT_LOGO_URL = 'https://centerconnect.co.za/centreconnect-logo-email.png'
 
 function BrandLogoMark({ size = 30 }: { size?: number }) {
   return (
